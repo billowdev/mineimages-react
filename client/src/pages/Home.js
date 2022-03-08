07d3d9c2a898c8e9fca631d25e094c55d89a4936
Akkarapon P <lacakkarapon@gmail.com>
2022-03-08 21:49:25 +0700

    demo design

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import MainComponent from '../components/Home/MainComponent'
+import React from "react";
+import "../App.css"
+import NavbarComponent from "../components/header/NavbarComponent";
+import MainComponent from "../components/Home/MainComponent";
 function Home() {
   return (
-	<div>
-		
-		<MainComponent />
-	</div>
-  )
+    <div>
+      <MainComponent></MainComponent>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
